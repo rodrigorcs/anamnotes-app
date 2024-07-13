913f7e547cc943efc2bd9469005af866ad392c69
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-07-13 19:35:55 -0300

    WIP: Added cognito token to websocket

--- src/apis/anamnotesWebsocket.ts
+++ src/apis/anamnotesWebsocket.ts
@@ -1,5 +1,5 @@
 import { IConversationWithSummarizations } from '../models/contracts/Conversations'
-import { convertConversationContract } from './common'
+import { convertConversationContract, getAuthTokenFromCognitoSession } from './common'
 import {
   EWebsocketMessageTypes,
   IWebsocketMessage,
@@ -15,17 +15,27 @@ export class AnamnotesWebsocketAPI {
     this.baseURL = VITE_ANAMNOTES_WEBSOCKET_BASE_URL
   }
 
-  private createWSConnection(conversationId: string) {
+  private createWSConnection({
+    conversationId,
+    authToken,
+  }: {
+    conversationId: string
+    authToken?: string
+  }) {
     const wsURL = new URL(this.baseURL)
+
     wsURL.searchParams.append('conversationId', conversationId)
+    if (authToken) {
+      wsURL.searchParams.append('idToken', authToken)
+    }
 
     return new WebSocket(wsURL)
   }
 
   public async getSummarizationMessage(conversationId: string) {
-    return new Promise<IConversationWithSummarizations>((resolve, reject) => {
-      const wsConnection = this.createWSConnection(conversationId)
-
+    return new Promise<IConversationWithSummarizations>(async (resolve, reject) => {
+      const authToken = await getAuthTokenFromCognitoSession()
+      const wsConnection = this.createWSConnection({ conversationId, authToken })
       wsConnection.onmessage = (event) => {
         const message = JSON.parse(event.data) as IWebsocketMessage<unknown>
         if (message.type !== EWebsocketMessageTypes.SUMMARIZATION) return
@@ -41,8 +51,9 @@ export class AnamnotesWebsocketAPI {
         }
       }
 
-      wsConnection.onerror = (error) => {
-        reject(error)
+      wsConnection.onerror = (event) => {
+        console.error(event)
+        reject(event)
       }
     })
   }
