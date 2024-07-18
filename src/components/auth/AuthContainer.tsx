8de911d1e6f8bf2ccbfaed4bad825060a9630064
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-07-18 09:50:44 -0300

    Added auth breakpoints

--- src/components/auth/AuthContainer.tsx
+++ src/components/auth/AuthContainer.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import signUpImage from '/assets/images/sign-up.png'
 import { useAuthStore } from '../../stores/auth'
+import { cn } from '../../utils/className'
 
 export const AuthContainer: FC = () => {
   const navigate = useNavigate()
@@ -13,11 +14,16 @@ export const AuthContainer: FC = () => {
   }, [user])
 
   return (
-    <div className="tw-flex-1 tw-flex tw-p-4 tw-justify-end">
-      <div className="tw-flex-1 tw-flex tw-pl-24 tw-pr-28 tw-pt-16 tw-py-28 tw-max-w-[42rem]">
+    <div className="tw-flex-1 tw-flex tw-p-4 tw-justify-end max-lg:tw-justify-center">
+      <div
+        className={cn(
+          'tw-flex-1 tw-flex tw-pl-24 tw-pr-28 tw-pt-16 tw-pb-28 tw-max-w-[42rem]',
+          'max-sm:tw-pl-12 max-sm:tw-pr-12 max-sm:tw-pb-16',
+        )}
+      >
         <Outlet />
       </div>
-      <div className="tw-flex-1 tw-flex tw-flex-col tw-bg-[#EDF2FF] tw-max-w-[56rem]">
+      <div className="tw-flex-1 tw-flex tw-flex-col tw-bg-[#EDF2FF] tw-max-w-[56rem] max-lg:tw-hidden">
         <h2 className="tw-text-brand-500 tw-text-4xl tw-leading-[3rem] tw-px-24 tw-mt-16 tw-text-center">
           Resuma as suas anamneses <span className="tw-font-semibold">automaticamente</span>.
         </h2>
