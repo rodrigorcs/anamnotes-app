28750ade05514a158c66bee88abeaff248bd83e1
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-07-18 10:36:53 -0300

    Updaetd centering

--- src/components/auth/AuthContainer.tsx
+++ src/components/auth/AuthContainer.tsx
@@ -17,12 +17,14 @@ export const AuthContainer: FC = () => {
     <div className="tw-flex-1 tw-flex tw-p-4 tw-justify-end max-lg:tw-justify-center">
       <div
         className={cn(
-          'tw-flex-1 tw-flex tw-pl-24 tw-pr-28 tw-py-16 tw-max-w-[42rem]',
+          'tw-flex-1 tw-pl-24 tw-pr-28 tw-py-16 tw-max-w-[42rem]',
           'max-sm:tw-pl-14 max-sm:tw-pr-14',
           'max-xs:tw-pl-4 max-xs:tw-pr-4',
         )}
       >
-        <Outlet />
+        <div className="tw-flex tw-flex-col tw-relative tw-top-[calc(50%-4.75rem)] tw-transform tw-translate-y-[calc(-50%+2.375rem)]">
+          <Outlet />
+        </div>
       </div>
       <div className="tw-flex-1 tw-flex tw-flex-col tw-bg-[#EDF2FF] tw-max-w-[56rem] max-lg:tw-hidden">
         <h2 className="tw-text-brand-500 tw-text-4xl tw-leading-[3rem] tw-px-24 tw-mt-16 tw-text-center">
